861070f149b3589b7615acb62ea44298841f54d7
Ethan Jucovy <ethan.jucovy@gmail.com>
2013-12-28 12:37:37 -0500

    tab to switch characters

--- game.js
+++ game.js
@@ -1,5 +1,27 @@
-var Q = Quintus({ development: true }).include("Sprites, Scenes, Input, 2D")
+Quintus.MyAI = function(Q) {
+    Q.component("myAI", {
+        init: function(p) {
+            this.on("bump.left", this, "jumpLeft");
+            this.on("bump.right", this, "jumpRight");
+        },
+        jump: function(collision) {
+            this.p.vy -= 300;
+        },
+        jumpLeft: function(collision) {
+            if( this.p.vx > 0 && this.p.vy == 0 ) { this.jump(collision); }
+        },
+        jumpRight: function(collision) {
+            if( this.p.vx < 0  && this.p.vy == 0 ) { this.jump(collision); }
+        }
+    });
+};
+
+var Q = Quintus({ development: true }).include("Sprites, Scenes, Input, 2D, MyAI")
   .setup({ height: 500, width: 500, maximize: false }).controls();
+
+Q.input.keyboardControls({
+    9: "tab"
+});
 
 Q.Sprite.extend("Player", {
     init: function(p) {
@@ -18,29 +40,33 @@ Q.Sprite.extend("Player", {
 Q.Sprite.extend("Archer", {
     init: function(p) {
         this._super(p, { asset: "archer.png", vx: 100 });
-        this.add("2d, aiBounce");
-        this.on("bump.left", this, "jumpLeft");
-        this.on("bump.right", this, "jumpRight");
-    },
-    jump: function(collision) {
-        this.p.vy -= 300;
-    },
-    jumpLeft: function(collision) {
-        if( this.p.vx > 0 && this.p.vy == 0 ) { this.jump(collision); }
-    },
-    jumpRight: function(collision) {
-        if( this.p.vx < 0  && this.p.vy == 0 ) { this.jump(collision); }
-    },
+        this.add("2d, aiBounce, myAI");
+    }
 });
     
 Q.scene("level1", function(stage) {
     stage.collisionLayer(new Q.TileLayer({ dataAsset: 'level.json', sheet: 'tiles' }));
 
-    stage.insert(new Q.Archer({ x: 700, y: 0 }));
-    stage.insert(new Q.Archer({ x: 900, y: 0 }));
+    var archer1 = new Q.Archer({ x: 700, y: 0 }); 
+    stage.insert(archer1);
+    var archer2 = new Q.Archer({ x: 900, y: 0 })
+    stage.insert(archer2);
     var player = new Q.Player({ x: 510, y: 90 })
     stage.insert(player);
     stage.add("viewport").follow(player);
+
+    var people = [archer1, archer2, player];
+    Q.input.on("tab", function() {
+        stage.viewport.following.del("platformerControls");
+        stage.viewport.following.add("myAI, aiBounce");
+        if( stage.viewport.following == player ) {
+            stage.viewport.following = archer1;
+        } else {
+            stage.viewport.following = people[people.indexOf(stage.viewport.following) + 1];
+        }
+        stage.viewport.following.del("myAI, aiBounce");
+        stage.viewport.following.add("platformerControls");
+    });
 });
 
 Q.load("angel.png, archer.png, level.json, tiles.png", function() {
